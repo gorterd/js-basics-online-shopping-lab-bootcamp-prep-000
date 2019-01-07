18ad5bb5e1cd77b443fce5ccc74b90f67daeedc7
Daniel Gorter <danieljgorter@gmail.com>
2019-01-07 19:46:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -49,5 +49,9 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if 
+  if (cardNumber === undefined) return 'Sorry, we don\'t have a credit card on file for you.';
+  var total = total();
+  cart = [];
+  return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`;
+  
 }
